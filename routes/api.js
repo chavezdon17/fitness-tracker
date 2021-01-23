7e5deeeccd93d52eb1edd92dee09c52efa3329e7
Donovan Chavez <chavezdon17@gmail.com>
2021-01-23 00:18:50 -0600

    added delete and post routes

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { db } = require("../models/workout.js");
 const Workout = require("../models/workout.js");
 
 router.post("/api/workouts", ({ body }, res) => {
@@ -16,6 +17,26 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
   console.log(body);
   const workoutId = params.id;
   let savedExercises = [];
+});
+
+router.post("api/workouts", (req, res) => {
+  db.Workout.create(req.body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.delete("/api/workouts", ({ body }, res) => {
+  Workout.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 module.exports = router;
