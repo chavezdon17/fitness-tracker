82c193d0936d3bceb34cc5ffb270182ba69501c7
Donovan Chavez <chavezdon17@gmail.com>
2021-01-23 18:47:15 -0600

    added put and get routes

--- routes/api.js
+++ routes/api.js
@@ -13,10 +13,29 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
+router.get("api/workouts", (req, res) => {
+  Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
 router.put("/api/workouts/:id", ({ body, params }, res) => {
   console.log(body);
-  const workoutId = params.id;
-  let savedExercises = [];
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercise: body } },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 router.post("api/workouts", (req, res) => {
